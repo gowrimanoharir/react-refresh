c4d010c37dc105dd0344da08f5940a5ee0874803
gowrimanoharir <gowrimanohari.r@gmail.com>
2019-05-30 18:36:13 -0400

    added parcel bundler

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-//pass props, its destructured here
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", { id: "try" }, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Bichon" }),
+    React.createElement(Pet, { name: "Lunar", animal: "Dog", breed: "Bichon" }),
     React.createElement(Pet, { name: "Grumpy", animal: "Cat", breed: "Mixed" }),
     React.createElement(Pet, { name: "Rain", animal: "Dog", breed: "Maltese" })
   ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
---
+++ src/Pet.js
@@ -0,0 +1,11 @@
+import React from "react";
+//pass props, its destructured here
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+export default Pet;
