43ff82539340effdb60b0e7f5006960e14e06d18
gowrimanoharir <gowrimanohari.r@gmail.com>
2019-06-04 19:01:18 -0400

    convert to jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "try" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Lunar", animal: "Dog", breed: "Bichon" }),
-    React.createElement(Pet, { name: "Grumpy", animal: "Cat", breed: "Mixed" }),
-    React.createElement(Pet, { name: "Rain", animal: "Dog", breed: "Maltese" })
-  ]);
+  // return React.createElement("div", { id: "try" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, { name: "Lunar", animal: "Dog", breed: "Bichon" }),
+  //   React.createElement(Pet, { name: "Grumpy", animal: "Cat", breed: "Mixed" }),
+  //   React.createElement(Pet, { name: "Rain", animal: "Dog", breed: "Maltese" })
+  // ]);
+  return (
+    <div id="try">
+      <h1>Adopt Me!</h1>
+      <Pet name="Lunar" animal="Dog" breed="Bichon" />
+      <Pet name="Grumpy" animal="Cat" breed="Mixed" />
+      <Pet name="Rain" animal="Dog" breed="Maltese" />
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
